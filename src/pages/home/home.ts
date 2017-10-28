2f747610e8fc591782648cacbf9d12d3e07b42d5
Ramon Castan <rcastan@br.ibm.com>
2017-10-28 17:51:50 -0200

    changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Toggle, AlertController } from 'ionic-angular';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  username: string;
+  usernameConfirmation: string;
+  password: string;
+  fullName: string;
+  mobilePhone: string;
+  livesPlace: string;
+  profilePicture: string;
+
+  checkbox: boolean;
+  
+
+  constructor(public navCtrl: NavController, 
+              public alertCtrl: AlertController) {
 
   }
+
+  ionViewWillEnter() {
+   
+   let input = document.getElementById('livesplace').getElementsByTagName('input')[0];
+   let autocomplete = new google.maps.places.Autocomplete(input, {types: ['geocode']});
+   google.maps.event.addListener(autocomplete, 'place_changed', () => {
+     let place = autocomplete.getPlace();
+     this.livesPlace = place.address_components[0].long_name;
+   });
+}
+
+onToggle(toggle: Toggle){
+  this.checkbox = toggle.checked;
+}
+
+  onConfirm(){
+    if(this.checkbox){
+    let user = new User(this.username, this.usernameConfirmation,
+                        this.password, this.fullName, this.mobilePhone,
+                        this.livesPlace, this.profilePicture);
+
+    console.log(JSON.stringify(user));
+  }else{
+    let alert = this.alertCtrl.create({
+      title: "Erro",
+      subTitle:'Para continuar você deve aceitar os termos e condições.',
+      buttons:['Ok']
+    });
+    alert.present();
+  }
+  }
+
+
+
+   
 
 }
