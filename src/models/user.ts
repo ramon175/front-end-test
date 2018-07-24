e9a4dff6d49374c209be5ba0403bedce254e5d1e
Ramon Castan <ramon.castan@ibm.com>
2018-07-24 10:52:16 -0300

    ussr model

---
+++ src/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+
+    constructor (public username:string, public usernameConfirmation:string,public password:string,
+        public fullname: string,public mobilePhone:string,public livesplace:string,public profilePicture:string){
+
+    }
+
+}
